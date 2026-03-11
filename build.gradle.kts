dede7d1b7abfa35c4590b5252a3224ceba0a9928
tn <prtnog@gmail.com>
2026-03-12 02:42:34 +0900

    feat: Add CI workflow and editorconfig for consistent code style and linting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 plugins {
     kotlin("jvm") version "2.2.0" apply false
     kotlin("plugin.serialization") version "2.2.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.3.1")
+        android.set(false)
+        outputToConsole.set(true)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+    }
 }
